5d1c7c59577ac46ee3cd51499a7fae00b86421c6
Allen D. Ball <ball@hcf.dev>
2017-09-04 18:15:11 +0000

    Initial writing (provided as template).
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.databind/trunk@2374 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/databind/ModuleImpl.java
@@ -0,0 +1,36 @@
+/*
+ * $Id$
+ *
+ * Copyright 2017 Allen D. Ball.  All rights reserved.
+ */
+package ball.databind;
+
+import ball.annotation.ServiceProviderFor;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+/**
+ * {@link Module} service provider for {@link ball.databind}.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+@ServiceProviderFor({ Module.class })
+public class ModuleImpl extends SimpleModule {
+    private static final long serialVersionUID = 6899958736994041726L;
+
+    /**
+     * Sole constructor.
+     */
+    public ModuleImpl() {
+        super(ModuleImpl.class.getPackage().getName());
+    }
+
+    @Override
+    public void setupModule(Module.SetupContext context) {
+        super.setupModule(context);
+    }
+
+    @Override
+    public String toString() { return super.toString(); }
+}
