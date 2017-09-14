8ceeb3cabab7fb3f97abb1370da4a0f67322e140
Allen D. Ball <ball@hcf.dev>
2017-09-14 00:49:11 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.databind/trunk@2388 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/databind/AbstractObjectMapperConfiguration.java
@@ -0,0 +1,107 @@
+/*
+ * $Id$
+ *
+ * Copyright 2017 Allen D. Ball.  All rights reserved.
+ */
+package ball.databind;
+
+import ball.io.IOUtil;
+import ball.util.EnumLookupMap;
+import ball.util.PropertiesImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.InputStream;
+import java.util.SortedMap;
+
+import static java.util.Collections.unmodifiableSortedMap;
+
+/**
+ * Abstract base class for {@link ObjectMapper} configurations.  The typical
+ * implementation is a trivial subclass combined with a corresponding
+ * {@link java.util.Properties} file.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+public abstract class AbstractObjectMapperConfiguration
+                      extends PropertiesImpl {
+    private static final long serialVersionUID = 3380012687021883148L;
+
+    /**
+     * Features supported by this configuration.
+     */
+    public static enum Feature {
+        /**
+         * If set and {@code true}, calls
+         * {@link ObjectMapper#registerModules(Iterable)} with the return
+         * value of {@link ObjectMapper#findModules(ClassLoader)}.
+         */
+        REGISTER_MODULES;
+
+        private static final SortedMap<String,Enum<?>> MAP =
+            unmodifiableSortedMap(new EnumLookupMap(Feature.class));
+    }
+
+    /**
+     * Sole constructor.
+     */
+    protected AbstractObjectMapperConfiguration() {
+        super();
+
+        String name = getClass().getSimpleName() + ".properties";
+        InputStream in = getClass().getResourceAsStream(name);
+
+        if (in != null) {
+            try {
+                load(in);
+            } catch (Exception exception) {
+                throw new ExceptionInInitializerError(exception);
+            } finally {
+                IOUtil.close(in);
+            }
+        }
+    }
+
+    /**
+     * Method to configure an {@link ObjectMapper}.
+     *
+     * @param   mapper          The {@link ObjectMapper} to configure.
+     *
+     * @return  The {@link ObjectMapper}.
+     */
+    public ObjectMapper configure(ObjectMapper mapper) {
+        for (String name : stringPropertyNames()) {
+            if (ObjectMapperFeature.MAP.containsKey(name)) {
+                ObjectMapperFeature
+                    .configure(mapper,
+                               ObjectMapperFeature.MAP.get(name),
+                               Boolean.valueOf(getProperty(name)));
+            }
+        }
+
+        for (String name : stringPropertyNames()) {
+            if (Feature.MAP.containsKey(name)) {
+                switch ((Feature) Feature.MAP.get(name)) {
+                case REGISTER_MODULES:
+                    ClassLoader loader = getClass().getClassLoader();
+
+                    mapper.registerModules(ObjectMapper.findModules(loader));
+                    break;
+
+                default:
+                    break;
+                }
+            }
+        }
+
+        return mapper;
+    }
+
+    /**
+     * Method to return a new, configured {@link ObjectMapper}.
+     *
+     * @return  The {@link ObjectMapper}.
+     */
+    public ObjectMapper newObjectMapper() {
+        return configure(new ObjectMapper());
+    }
+}
