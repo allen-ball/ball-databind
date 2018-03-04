eeeb6d728df414fa6fa2b992e759ea50ac361314
Allen D. Ball <ball@hcf.dev>
2018-03-04 01:09:06 +0000

    Implement Serializable.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.databind/trunk@2523 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/databind/JSONBean.java
+++ src/main/java/ball/databind/JSONBean.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Copyright 2017 Allen D. Ball.  All rights reserved.
+ * Copyright 2017, 2018 Allen D. Ball.  All rights reserved.
  */
 package ball.databind;
 
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.Serializable;
 
 /**
  * Abstract base class for bean implementations that wrap a
@@ -18,7 +19,8 @@ import com.fasterxml.jackson.databind.SerializationFeature;
  * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
  * @version $Revision$
  */
-public abstract class JSONBean {
+public abstract class JSONBean implements Serializable {
+    private static final long serialVersionUID = 7361248610078338176L;
 
     /**
      * Unconfigured {@link ObjectMapper} available for implementing subclass
