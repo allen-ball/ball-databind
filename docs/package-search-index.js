3fe5663097a6ca70bbe5b936636dc8e5af4d042b
Allen D. Ball <ball@hcf.dev>
2022-10-22 13:07:21 -0700

    Added pom.xml and generated javadoc.

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ball.databind"},{"l":"ball.databind.ant"},{"l":"ball.databind.ant.taskdefs"},{"l":"ball.databind.entity"}]
